dddc8c904bf63c98182ffac2ee30b968b4ee28ad
heisenberg <sda@example.com>
2023-03-13 20:18:15 +0800

    s

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const port = process.env.PORT || 8080;
 connect();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:"100mb"}));
+app.use(express.urlencoded({limit: '100mb'}));
 
 app.use(userRoutes);
 app.use(commentRoutes);
