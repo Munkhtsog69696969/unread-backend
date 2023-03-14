582230614f888877b576c6ddedc30c8e0580109f
22hop2d-Munkhtsog <22hop2d-Munkhtsog@LEAP-10s-iMac.local>
2023-03-14 15:45:31 +0800

    getPosts'ngetPost

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,16 +1,16 @@
-const express=require("express");
+const express = require("express");
 
-const {createPost}=require("../controller/post.controller");
+const { createPost } = require("../controller/post.controller");
 
-const {getPosts}=require("../controller/post.controller")
+const { getPosts } = require("../controller/post.controller")
 
-const {getPost}=require("../controller/post.controller");
+const { getPost } = require("../controller/post.controller");
 
-const router=express.Router();
+const router = express.Router();
 
 router
-    .get("/getPost",getPosts)
-    .get("/post/:id",getPost)
-    .post("/createPost",createPost)
+    .get("/getPosts", getPosts)
     .get("/getPost/:id", getPost)
-module.exports.postRoutes=router;
+    .post("/createPost", createPost)
+    .get("/getPost/:id", getPost)
+module.exports.postRoutes = router;
